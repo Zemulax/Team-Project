cb7c80f51e967b042111e73678f0bf2344a370a1
Zemulax <100323567+Zemulax@users.noreply.github.com>
2023-03-14 19:57:53 +0000

    Improved temp reading & changed disp size

--- dht.py
+++ dht.py
@@ -4,31 +4,42 @@ import adafruit_dht #sensor library
 
 #limit the time the sensor can be read
 period = 30 #seconds
+line_number = 2 #line number to start reading from
 
 #Initialize the sensor, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D4) #depends on where the signal cable is plugged
 
 #open a file to write to
-with open('Temperature_Humidity.log', 'w') as f:
+with open('Temperature_Humidity.log', 'r+') as f:
+  
+  lines = f.readlines() #read the file
+  
+  if not lines: #if the file is empty
+    lines.append("Temperature\n") #add a header to the file
+    
     #start the timer
     start_time = time.monotonic()
-
     #loop until the time limit is reached
     while time.monotonic() - start_time < period:  #time wil not go backwards
-       try:
-         # get the readings from the sensor
-         temperature_c = dhtDevice.temperature
-         temperature_f = temperature_c * (9 / 5) + 32
-         humidity = dhtDevice.humidity
+          try:
+                  # get the readings from the sensor
+                  temperature_c = dhtDevice.temperature 
+                  temperature_f = temperature_c * (9 / 5) + 32
+                  humidity = dhtDevice.humidity#
+                  
+                  #write the readings to the file
+                  lines [line_number-1] = f.write("Temperature: {:.1f} F / {:.1f} C\n"
+                  "Humidity: {:.1f}%\n\n"
+                  .format(temperature_f, temperature_c, humidity))
+                  f.seek(0) #go to the beginning of the file
+                  f.writelines(lines)
+                  f.truncate() #delete the rest of the file
+                  
+                  #to make sure the file is written to
+                  f.flush()
+          except RuntimeError as error:pass #skip error messages
+          time.sleep(2.0) #get measurements every 2 seconds
+    
          
-            #write the readings to the file
-         f.write("Temperature: {:.1f} F / {:.1f} C\n"
-                 "Humidity: {:.1f}%\n\n"
-               .format(temperature_f, temperature_c, humidity))
-         
-         #to make sure the file is written to
-         f.flush()
-       except RuntimeError as error:pass #skip error messages
-       time.sleep(2.0) #get measurements every 2 seconds
 
 print("Temperature reading completed") #the program is done
--- display.py
+++ display.py
@@ -1,14 +1,14 @@
 # Description: This program displays the contents of the log file in a GUI window
-#the log files comes from the dht.py program
                                
 import tkinter as tk
 import asyncio
 import os
+from tkinter import *
 
 #create a new window for displaying the file contents from the log
 window = tk.Tk()
 window.title("Sensor Log")
-window.geometry("280x600")
+window.geometry("350x100")
 window.configure(bg="purple")
 window.configure(borderwidth=10, relief="groove")
 window.resizable(width=False, height=False)
@@ -16,7 +16,6 @@ window.resizable(width=False, height=False)
 #display the file content here
 text_widget = tk.Text(window)
 text_widget.pack(fill="both", expand=True)
-
 #read cotents from the log
 async def read_file(filename):
      while True:
@@ -24,17 +23,19 @@ async def read_file(filename):
               for line in file:
                      #insert the file content into the widget
                      text_widget.insert("end", line)
-                     text_widget.update() #update the widget with new information
+                     text_widget.update() #dynamically update the widget with new information
               await asyncio.sleep(3) #wait 3 seconds before reading the file again
+              text_widget.delete("1.0", "end")
 
 async def main():
     filename = "/home/pi/Desktop/Temperature_Humidity.log"
     if os.path.exists(filename): #returns true if file exists
          await read_file(filename)
-    else: text_widget.insert("end","""Sensor log was not found!!\nIs the sensor running?\n"""
-                             """Restart this program when the sensor is operational.""")
+    else:
+        text_widget.insert("end","""Sensor log was not found!!\nIs the sensor running?\n"""
+                             """Restart this program when the sensor is operational""")
+        text_widget.configure(state="disabled") #disbaled editing
 
 if __name__ == '__main__':
      asyncio.run(main())
-     window.mainloop() #display the window
-
+     window.mainloop() #display the main window
