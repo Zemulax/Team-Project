fe341762bcec5e2d2ad1247fccff58b0f6f23b6a
Moses Harazi <100323567+Zemulax@users.noreply.github.com>
2023-05-03 20:32:11 +0100

    Final files
    These three are the final files for the program. Hygrometer is the executable file that can be run independently, while hygrometer.py and hygrometergui.py are two scripts that run together and should be placed in the same folder, call it "Hygrometer", and create another folder in /pi/home/ called TemperatureFolder as this is where the log file is kept. However, the code can be modified to suit one's needs, but the above is just a provisional method for running the program with the sensor.

---
+++ hygrometer.py
@@ -0,0 +1,109 @@
+import time
+import board #define the gpio pin in use
+import adafruit_dht #sensor library 
+import fcntl #file locking library
+import os #operating system library
+from datetime import datetime #date and time library
+import threading #threading library
+import subprocess #subprocess library
+import sys
+
+#constants
+FILE_NAME = "/home/pi/TemperatureFolder/Temperature_Humidity.log"
+PIN_NUMBER = board.D4
+HEADER_LINE = "||||Today's Weather Report||||\n\n"
+INFO = "" #add an empty line
+
+#variables
+line_number = 2 #line number to start reading from
+dhtSensor = adafruit_dht.DHT11(PIN_NUMBER) #Initialize the sensor, with data pin connected to: PIN_NUMBER
+
+#this function simply exits the program
+#upon specified error occurrence
+def quit():
+  sys.exit(0)
+  
+#function to call the display.py file
+#this function is called in a thread
+#so that the program can continue to read the temperature
+#while the display is reading from the file
+def call_display():
+    #subprocess.call(["python3", "/home/pi/Hygrometer/hygrometergui.py"]) #call the subprocess
+    subprocess.call(["python3", os.path.join(sys._MEIPASS, "hygrometergui.py")]) #call the subprocess when creating executable use this line
+
+#function to write errors to a file
+def error_reporting(error):
+    with open("/home/pi/TemperatureFolder/Temperature_Humidity.log", 'w') as file:
+      file.truncate()
+      file.write(error)
+      file.flush()
+      
+#function to read temperature from the sensor
+#and write it to a file
+#the file is locked to prevent data corruption
+#the file is flushed to clear the buffer
+def read_temperature():
+    #create or open a file with read and right permissions
+    with open(FILE_NAME, 'w+') as file:
+
+      lines = file.readlines() #read the filelines into a list called lines
+
+      if not lines: #if the file is empty ->
+        file.write(INFO) #add a header line as starting point
+        lines.append(HEADER_LINE) #add a header to the list
+
+        #loop forever
+        while True:
+              try:
+                      fcntl.flock(file,fcntl.LOCK_SH) #lock the file
+                      
+                      # assign temp reading to variables
+                      temperature_c = dhtSensor.temperature 
+                      temperature_f = temperature_c * (9/5) + 32
+                      humidity = dhtSensor.humidity 
+
+                      #assign temp reading to the lines, replacing their current text
+                      if len(lines) >= line_number:
+                           lines [line_number-1] = f"Temperature\n{temperature_f:.1f} F | {temperature_c:.1f}°C\n\nHumidity\n{humidity:.1f}%"
+                      else:
+                          lines.append(f"Temperature\n{temperature_f:.1f} F | {temperature_c:.1f}°C\n\nHumidity\n{humidity:.1f}%")
+                      
+                      file.seek(0) #go to the beginning of the file
+                      file.writelines(lines) # write line to the file
+                      fcntl.flock(file, fcntl.LOCK_UN) #unlock the file
+                      file.truncate() #delete the rest of the file
+                      file.flush() #flush the buffer
+                      time.sleep(5.0) #get measurements every 2 secon
+              
+              #catch errors
+              except RuntimeError as error: 
+                     error_reporting("Awaiting Sensor...") #print error messages to the display but continue executing the program
+                     time.sleep(30)
+                     continue
+                    
+              except TypeError:
+                     file.write("Unable to Read from Sensor")
+                     time.sleep(10)
+                     continue
+                      
+              except OverflowError:
+                     error_reporting("Halted.Connect Sensor and Restart Program")
+                     time.sleep(20)
+                     quit()
+       
+              except KeyboardInterrupt:
+                     dhtSensor.exit()
+                     quit()
+
+      dhtSensor.exit() #close the sensor        
+
+def main():
+  thread = threading.Thread(target=call_display) #create a thread
+  thread.start() #start the thread
+  read_temperature() #call the read_temperature function
+
+#call the main function
+#when the program is executed
+#this is the entry point of the program
+if __name__ == "__main__":
+  main() #call the main function
---
+++ hygrometergui.py
@@ -0,0 +1,103 @@
+# Description: This program displays the contents of the log file in a GUI window
+                               
+import tkinter as tk #tkinter library
+import os #operating system library
+import fcntl #file locking library
+import time #time library
+import threading #threading library
+from queue import Queue #queue library
+import sys
+
+#this class creates a window for displaying the file contents
+#it also creates a thread for reading the file
+#and a function for updating the text widget
+#the thread is started in the main function
+#the update function is called in the mainloop
+#the update function is called periodically
+class DispClass(tk.Frame):
+     #initialize the class
+    def __init__(self, master=None):
+        super().__init__(master)
+        frame = tk.Frame(self.master)
+        frame.pack(fill="both", expand=True)
+
+        # create a text widget for displaying the file contents
+        self.text_widget = tk.Text(frame, bg="midnightblue", fg= "white",font=("Arial", 16))
+        self.text_widget.pack(fill="both", expand=True)
+        self.label = tk.Label(frame, text="Reading Temperature...", fg="white")
+        self.label.pack()
+        
+     #function for reading the file
+     #the file is locked to prevent data corruption
+     #the file is unlocked to allow other processes to access it
+    def read_file(self, q, filename):
+        while True:
+            try:
+                with open(filename, "r") as file:
+                     # lock the file
+                    fcntl.flock(file, fcntl.LOCK_SH)
+
+                    for line in file:
+                        # insert the file content into the queue
+                        q.put(line)
+                    #unlock the file
+                    fcntl.flock(file, fcntl.LOCK_UN)
+
+                time.sleep(10) #wait 10 seconds before reading the file again
+                # clear the text widget
+                self.text_widget.delete("1.0", "end")
+            
+            except tk.TclError as error:  # program will know that its been closed on purpose
+                quit("Program was closed")
+                
+
+#main function
+#creates a new window for displaying the file contents
+#creates a new window for displaying the error message
+#starts the thread
+#calls the update function periodically
+#calls the mainloop
+#the mainloop is called in the main function
+def main():
+# create a new window for displaying the file contents from the log
+    window = tk.Tk()
+    window.title("Hygrometer")
+    window.geometry("307x210")
+    window.configure(bg="white")
+    window.configure(borderwidth=10, relief="sunken")
+    window.resizable(width=False, height=False)
+
+    #icon = tk.PhotoImage(file="home/pi/Desktop/TestFolder/magni/icon.png")
+    #window.iconphoto(True, icon)
+
+    disp = DispClass(master=window)
+    disp.pack(fill="both", expand=True)
+    
+    filename = "/home/pi/TemperatureFolder/Temperature_Humidity.log"
+    if os.path.exists(filename):  # returns true if file exists
+        
+
+        q = Queue()
+        t = threading.Thread(target=disp.read_file, args=(q, filename))
+        t.start()
+
+        def update_text_widget():
+            while not q.empty():
+                line = q.get_nowait()
+                disp.text_widget.insert("end",line)
+                disp.text_widget.see("end")
+            window.after(500, update_text_widget)
+            
+        window.after(500, update_text_widget)
+        window.mainloop()
+        
+    else:
+
+        disp.text_widget.insert("end", """Sensor log was not found!!\nIs the sensor running?\n"""
+                                       """Restart this program when the sensor is operational""")
+        disp.text_widget.configure(state="disabled")  # disabled editing
+
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
